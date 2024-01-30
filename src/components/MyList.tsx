65b67ba864e82ce4f5ceceb00e7034ef12d64c47
Vadim Savin <savinvadim1312@gmail.com>
2024-01-30 16:00:23 +0000

    Basic Pagination

--- src/components/MyList.tsx
+++ src/components/MyList.tsx
@@ -1,33 +1,48 @@
 import character from '../data/character.json';
 import CharacterListItem from './CharacterListItem';
-import { ActivityIndicator, FlatList, Text } from 'react-native';
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
 import { useState, useEffect } from 'react';
 
 const MyList = () => {
   const [loading, setLoading] = useState(false);
   const [items, setItems] = useState([]);
+  const [nextPage, setNextPage] = useState(
+    'https://rickandmortyapi.com/api/character'
+  );
+
+  const fetchNextPage = async () => {
+    setLoading(true);
+    const response = await fetch(nextPage);
+    const responseJson = await response.json();
+
+    setItems((existingItems) => {
+      return [...existingItems, ...responseJson.results];
+    });
+    setNextPage(responseJson.info.next);
+    setLoading(false);
+  };
 
   useEffect(() => {
-    const fetchItems = async () => {
-      setLoading(true);
-      const response = await fetch('https://rickandmortyapi.com/api/character');
-      const responseJson = await response.json();
-      setItems(responseJson.results);
-      setLoading(false);
-    };
-
-    fetchItems();
+    fetchNextPage();
   }, []);
-
-  if (loading) {
-    return <ActivityIndicator />;
-  }
 
   return (
     <FlatList
       data={items}
       renderItem={({ item }) => <CharacterListItem character={item} />}
       contentContainerStyle={{ gap: 10 }}
+      ListFooterComponent={() => (
+        <View>
+          {loading && <ActivityIndicator />}
+
+          <Text
+            onPress={fetchNextPage}
+            style={{ alignSelf: 'center', fontSize: 20, color: 'blue' }}
+          >
+            Load more
+          </Text>
+        </View>
+      )}
     />
   );
 };
