7ff639bc464d443c1087255da59e0a7c026b00bf
Vadim Savin <savinvadim1312@gmail.com>
2024-01-30 15:26:13 +0000

    Basic FlatList

--- src/components/MyList.tsx
+++ src/components/MyList.tsx
@@ -1,8 +1,17 @@
 import character from '../data/character.json';
 import CharacterListItem from './CharacterListItem';
+import { FlatList, Text } from 'react-native';
 
 const MyList = () => {
-  return <CharacterListItem character={character.results[0]} />;
+  // return <CharacterListItem character={character.results[0]} />;
+
+  return (
+    <FlatList
+      data={character.results}
+      renderItem={({ item }) => <CharacterListItem character={item} />}
+      contentContainerStyle={{ gap: 10 }}
+    />
+  );
 };
 
 export default MyList;
